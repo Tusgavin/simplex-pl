57ec64cb5609fbf3f8817e8de8e30c947d425fba
Gustavo Dias de Oliveira <dias.gdo@gmail.com>
2021-07-14 12:09:18 -0300

    modularize code

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-def main():
-   number_of_restrictions, number_of_variables = [int(x) for x in input().split()]
+def pivot(A, b, c, leaving_index, entering_index):
+   print(A, b, c, leaving_index, entering_index)
 
+def getOptimizationInputValues(number_of_variables):
    c = [int(x) for x in input().split()]
 
    A = []
@@ -20,19 +21,38 @@ def main():
    A = np.array(A, dtype=np.int)
    b = np.array(b, dtype=np.int)
    c = np.array(c, dtype=np.int)
-   
+
+   return (A, b, c)
+
+def getFPIForm(A, b, c, number_of_restrictions):
    ident = np.identity(number_of_restrictions, dtype=np.int);
    new_vars = np.zeros((number_of_restrictions), dtype=np.int);
 
-   A = np.concatenate((A, ident), axis=1)
-   c = np.concatenate((c, new_vars), axis=None)
+   A_fpi = np.concatenate((A, ident), axis=1)
+   c_fpi = np.concatenate((c, new_vars), axis=None)
+   b_fpi = np.copy(b)
 
-   print(A)  
-   print(b)
-   print(c)
+   return (A_fpi, b_fpi, c_fpi)
 
+def main():
+   number_of_restrictions, number_of_variables = [int(x) for x in input().split()]
 
+   (A, b, c) = getOptimizationInputValues(number_of_variables)
 
+   (A_fpi, b_fpi, c_fpi) = getFPIForm(A, b, c, number_of_restrictions)
+
+   if (c <= 0).all():
+      print("otima")
+      valor_obj = np.dot(b, c);
+      print(valor_obj)
+      print(b)
+      print(c)
+
+   for index, element in enumerate(c):
+      if element > 0:
+         if (A[:,index] <= 0).all():
+            print("ilimitada")
+            print(b)
 
 if __name__ == "__main__":
    main()
